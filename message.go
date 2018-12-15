f6eb58fb4902c823b2e423e079151a7162de42ef
James Harris <james.harris@icecave.com.au>
2018-12-15 13:17:52 +1000

    Document the various categories of messages.

--- message.go
+++ message.go
@@ -1,6 +1,16 @@
 package dogma
 
-// Message is an application-defined unit of data.
+// A Message is an application-defined unit of data.
+//
+// Messages are divided into two broad categories; "domain messages", which
+// relate to the application's domain logic, and "integration messages" which
+// are used to integrate the domain with "non-domain concerns", such as
+// third-party APIs.
+//
+// Within each category, messages are further divided into "commands" and
+// "events". Command messages represent a request to perform some action,
+// whereas event messages represent some occurrance which has already taken
+// place.
 type Message interface {
 }
 
