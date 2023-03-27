eee9e400ad86731fefe20fc6aeb1a8ad6572e592
James Harris <contact@jamesharris.id.au>
2023-03-27 11:56:58 +1000

    Add `fixtures.Command`, `Event` and `Timeout`.

--- fixtures/message.go
+++ fixtures/message.go
@@ -1,5 +1,67 @@
 package fixtures
 
+import (
+	"errors"
+	"fmt"
+)
+
+// Command is an implementation of dogma.Command used for testing.
+type Command[T any] struct {
+	Content T
+	Invalid string
+}
+
+// MessageDescription returns a description of the command.
+func (c Command[T]) MessageDescription() string {
+	return fmt.Sprintf("%v", c)
+}
+
+// Validate returns a non-nil error if c.Invalid is not empty.
+func (c Command[T]) Validate() error {
+	if c.Invalid != "" {
+		return errors.New(c.Invalid)
+	}
+	return nil
+}
+
+// Event is an implementation of dogma.Event used for testing.
+type Event[T any] struct {
+	Content T
+	Invalid string
+}
+
+// MessageDescription returns a description of the command.
+func (e Event[T]) MessageDescription() string {
+	return fmt.Sprintf("%v", e)
+}
+
+// Validate returns a non-nil error if c.Invalid is not empty.
+func (e Event[T]) Validate() error {
+	if e.Invalid != "" {
+		return errors.New(e.Invalid)
+	}
+	return nil
+}
+
+// Timeout is an implementation of dogma.Timeout used for testing.
+type Timeout[T any] struct {
+	Content T
+	Invalid string
+}
+
+// MessageDescription returns a description of the command.
+func (t Timeout[T]) MessageDescription() string {
+	return fmt.Sprintf("%v", t)
+}
+
+// Validate returns a non-nil error if c.Invalid is not empty.
+func (t Timeout[T]) Validate() error {
+	if t.Invalid != "" {
+		return errors.New(t.Invalid)
+	}
+	return nil
+}
+
 // MessageA is type used as a dogma.Message in tests.
 type MessageA struct {
 	Value interface{}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/dogmatiq/dogma
 
-go 1.13
+go 1.19
