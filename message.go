84a2c5852613c85e7e2b5b527445aed8cc013c7f
James Harris <contact@jamesharris.id.au>
2024-08-17 18:05:15 +1000

    Add validation scope interfaces.

--- message.go
+++ message.go
@@ -1,46 +1,42 @@
 package dogma
 
+import "time"
+
 // A Message is an application-defined unit of data that describes a [Command],
 // [Event], or [Timeout] within a message-based application.
 //
-// Deprecated: Application developers should use [Command], [Event], or
-// [Timeout] instead.
+// Typically, application developers should use one of the more specific
+// [Command], [Event], or [Timeout] interface instead.
 type Message interface {
 	// MessageDescription returns a human-readable description of the message.
 	MessageDescription() string
-
-	// Validate returns a non-nil error if the message is invalid.
-	Validate() error
 }
 
 // A Command is a message that represents a request for a Dogma application to
 // perform some action.
 type Command interface {
-	// MessageDescription returns a human-readable description of the message.
-	MessageDescription() string
+	Message
 
 	// Validate returns a non-nil error if the message is invalid.
-	Validate() error
+	Validate(CommandValidationScope) error
 }
 
 // An Event is a message that indicates that some action has occurred within a
 // Dogma application.
 type Event interface {
-	// MessageDescription returns a human-readable description of the message.
-	MessageDescription() string
+	Message
 
 	// Validate returns a non-nil error if the message is invalid.
-	Validate() error
+	Validate(EventValidationScope) error
 }
 
 // A Timeout is a message that represents a request for an action to be
 // performed at a specific time.
 type Timeout interface {
-	// MessageDescription returns a human-readable description of the message.
-	MessageDescription() string
+	Message
 
 	// Validate returns a non-nil error if the message is invalid.
-	Validate() error
+	Validate(TimeoutValidationScope) error
 }
 
 // UnexpectedMessage is a panic value used by a message handler when it receives
@@ -48,3 +44,26 @@ type Timeout interface {
 var UnexpectedMessage unexpectedMessage
 
 type unexpectedMessage struct{}
+
+// CommandValidationScope provides information about the scope under which a
+// [Command] is being validated.
+type CommandValidationScope interface {
+	reserved()
+}
+
+// EventValidationScope provides information about the scope under which an
+// [Event] is being validated.
+type EventValidationScope interface {
+	// RecordedAt returns the time at which the event being validated was
+	// recorded.
+	//
+	// If the event has not yet been recorded, ok is false and the value of t is
+	// undefined.
+	RecordedAt() (t time.Time, ok bool)
+}
+
+// TimeoutValidationScope provides information about the scope under which a
+// [Timeout] is being validated.
+type TimeoutValidationScope interface {
+	reserved()
+}
