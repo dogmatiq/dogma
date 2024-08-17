6a3436f7be1e2f479ad4123d3205129281e0345e
James Harris <contact@jamesharris.id.au>
2024-08-18 08:14:46 +1000

    Mark `fixtures` package as deprecated.

--- fixtures/doc.go
+++ fixtures/doc.go
@@ -1,2 +1,6 @@
 // Package fixtures is a set of test fixtures and mocks of Dogma interfaces.
+//
+// Deprecated: Engine developers can use enginekit [stubs] instead.
+//
+// [stubs]: https://pkg.go.dev/github.com/dogmatiq/enginekit/enginetest/stubs
 package fixtures
