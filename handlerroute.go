d464a71299322eda3a68341c49c40f2eb5adea7d
James Harris <contact@jamesharris.id.au>
2025-06-21 17:44:03 +1000

    Simplify docs on ViaXXX() functions and HandlerRoute .

--- handlerroute.go
+++ handlerroute.go
@@ -1,82 +1,50 @@
 package dogma
 
-// ViaAggregate configures an [Application] to route messages to and from the
-// specified [AggregateMessageHandler]. It is used as an argument to the
-// Routes() method of [ApplicationConfigurer].
-//
-// [Command] messages executed using a [CommandExecutor], [ProcessEventScope] or
-// [ProcessTimeoutScope] are routed to h if it has a [HandlesCommandRoute] for
-// that command type.
-//
-// [Event] messages recorded by h using an [AggregateCommandScope] are routed to
-// other handlers according to their route configurations.
+// ViaAggregate configures the [Application] to route messages to and from an
+// [AggregateMessageHandler].
 func ViaAggregate(h AggregateMessageHandler, _ ...ViaAggregateOption) ViaAggregateRoute {
 	return ViaAggregateRoute{h}
 }
 
-// ViaProcess configures an [Application] to route messages to and from the
-// specified [ProcessMessageHandler]. It is used as an argument to the Routes()
-// method of [ApplicationConfigurer].
-//
-// [Event] messages recorded using an [AggregateCommandScope] or
-// [IntegrationCommandScope] are routed to h if it has a [HandlesEvent] route
-// for that event type.
-//
-// [Command] messages executed by h using a [ProcessEventScope] or
-// [ProcessTimeoutScope] are routed to other handlers according to their route
-// configurations.
-//
-// [Timeout] messages are always routed back to h itself.
+// ViaProcess configures the [Application] to route messages to and from a
+// [ProcessMessageHandler].
 func ViaProcess(h ProcessMessageHandler, _ ...ViaProcessOption) ViaProcessRoute {
 	return ViaProcessRoute{h}
 }
 
-// ViaIntegration configures an [Application] to route messages to and from the
-// specified [IntegrationMessageHandler]. It is used as an argument to the
-// Routes() method of [ApplicationConfigurer].
-//
-// [Command] messages executed using a [CommandExecutor], [ProcessEventScope] or
-// [ProcessTimeoutScope] are routed to h if it has a [HandlesCommandRoute] for
-// that command type.
-//
-// [Event] messages recorded by h using an [IntegrationCommandScope] are routed
-// to other handlers according to their route configurations.
+// ViaIntegration configures the [Application] to route messages to and from an
+// [IntegrationMessageHandler].
 func ViaIntegration(h IntegrationMessageHandler, _ ...ViaIntegrationOption) ViaIntegrationRoute {
 	return ViaIntegrationRoute{h}
 }
 
-// ViaProjection configures an [Application] to route messages to the specified
-// [ProjectionMessageHandler]. It is used as an argument to the Routes() method
-// of [ApplicationConfigurer].
-//
-// [Event] messages recorded using an [AggregateCommandScope] or
-// [IntegrationCommandScope] are routed to h if it has a [HandlesEvent] route
-// for that event type.
+// ViaProjection configures the [Application] to route messages to a
+// [ProjectionMessageHandler].
 func ViaProjection(h ProjectionMessageHandler, _ ...ViaProjectionOption) ViaProjectionRoute {
 	return ViaProjectionRoute{h}
 }
 
 type (
-	// HandlerRoute is an interface for all types that describe a relationship
-	// between an [Application] and the a handler.
+	// HandlerRoute is an interface for types that represent a relationship
+	// between the [Application] and a message handler.
 	HandlerRoute interface {
 		isHandlerRoute()
 	}
 
-	// ViaAggregateRoute describes an [AggregateMessageHandler] that is to be
-	// registered with an [Application].
+	// ViaAggregateRoute is a [HandlerRoute] that represents a relationship
+	// between the [Application] and an [AggregateMessageHandler].
 	ViaAggregateRoute struct{ Handler AggregateMessageHandler }
 
-	// ViaProcessRoute describes a [ProcessMessageHandler] that is to be
-	// registered with an [Application].
+	// ViaProcessRoute is a [HandlerRoute] that represents a relationship
+	// between the [Application] and a [ProcessMessageHandler].
 	ViaProcessRoute struct{ Handler ProcessMessageHandler }
 
-	// ViaIntegrationRoute describes an [IntegrationMessageHandler] that is
-	// to be registered with an [Application].
+	// ViaIntegrationRoute is a [HandlerRoute] that represents a relationship
+	// between the [Application] and an [IntegrationMessageHandler].
 	ViaIntegrationRoute struct{ Handler IntegrationMessageHandler }
 
-	// ViaProjectionRoute describes a [ProjectionMessageHandler] that is to be
-	// registered with an [Application].
+	// ViaProjectionRoute is a [HandlerRoute] that represents a relationship
+	// between the [Application] and a [ProjectionMessageHandler].
 	ViaProjectionRoute struct{ Handler ProjectionMessageHandler }
 )
 
