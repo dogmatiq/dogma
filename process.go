f807eae55ca48e5b96e69037f5cc7ebc8188b365
James Harris <contact@jamesharris.id.au>
2025-07-09 08:15:48 +1000

    Embed `HandlerConfigurer` in `ProcessConfigurer`.

--- process.go
+++ process.go
@@ -85,19 +85,7 @@ type ProcessRoot interface{}
 // A ProcessConfigurer configures the engine for use with a specific process
 // message handler.
 type ProcessConfigurer interface {
-	// Identity configures the handler's identity.
-	//
-	// n is a short human-readable name. It MUST be unique within the
-	// application at any given time, but MAY change over the handler's
-	// lifetime. It MUST contain solely printable, non-space UTF-8 characters.
-	// It must be between 1 and 255 bytes (not characters) in length.
-	//
-	// k is a unique key used to associate engine state with the handler. The
-	// key SHOULD NOT change over the handler's lifetime. k MUST be an RFC 4122
-	// UUID, such as "5195fe85-eb3f-4121-84b0-be72cbc5722f".
-	//
-	// Use of hard-coded literals for both values is RECOMMENDED.
-	Identity(n string, k string)
+	HandlerConfigurer
 
 	// Routes configures the engine to route certain message types to and from
 	// the handler.
@@ -105,17 +93,6 @@ type ProcessConfigurer interface {
 	// Process handlers support the HandlesEvent(), ExecutesCommand() and
 	// SchedulesTimeout() route types.
 	Routes(...ProcessRoute)
-
-	// Disable prevents the handler from receiving any messages.
-	//
-	// The engine MUST NOT call any methods other than Configure() on a disabled
-	// handler.
-	//
-	// Disabling a handler is useful when the handler's configuration prevents
-	// it from operating, such as when it's missing a required dependency,
-	// without requiring the user to conditionally register the handler with the
-	// application.
-	Disable(...DisableOption)
 }
 
 // ProcessEventScope performs engine operations within the context of a call
