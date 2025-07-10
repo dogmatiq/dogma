5946fd5cd5315b7df85d31770f04323eb1474dbb
James Harris <contact@jamesharris.id.au>
2025-07-10 14:46:22 +1000

    Remove RFC 2119 boilerplate from package docs.

--- doc.go
+++ doc.go
@@ -1,7 +1,3 @@
-// Package dogma contains the API used by a Dogma application to interact with a
-// Dogma engine.
-//
-// The key words "MUST", "MUST NOT", "REQUIRED", "SHALL", "SHALL NOT", "SHOULD",
-// "SHOULD NOT", "RECOMMENDED", "MAY", and "OPTIONAL" in this document are to be
-// interpreted as described in RFC 2119.
+// Package dogma defines the API for Dogma, a suite of tools for building robust
+// message-driven applications in Go.
 package dogma
